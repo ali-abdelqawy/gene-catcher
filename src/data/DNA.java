58913c29e0d764cb9c6de32bf45521aff20f0c85
ali-abdelqawy <ali.abdelqawy@outlook.com>
2021-08-26 01:30:14 +0200

    refactor data.DNA class

--- src/data/DNA.java
+++ src/data/DNA.java
@@ -1,9 +1,22 @@
 package data;
 
+import java.util.HashMap;
+
 public class DNA {
-	private String strand = "";
-	private String fileName = "";
 	final static String startCodon = "ATG";
 	final static String[] stopCodons = {"TAA", "TAG", "TGA"};
 	final static int codonLength = 3;
+	
+	public static HashMap<String, String> getStrandsByFiles(String folderName) {
+		Fetcher fetcher = new Fetcher(folderName);
+		return fetcher.getFilesContent();
+	}
+	
+	public static String getStartCodon() {
+		return startCodon;
+	}
+	
+	public static String[] getStopCodons() {
+		return stopCodons;
+	}
 }
--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,19 @@
 package main;
 
+import java.util.HashMap;
+
+import data.DNA;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("This is the gene catcher project!");
+		final String folderName = "input";
+		HashMap<String, String> strandsByFiles = DNA.getStrandsByFiles(folderName);
+	    for (HashMap.Entry<String, String> entry : strandsByFiles.entrySet()) {
+	        System.out.println(entry.getKey());
+	        System.out.println(entry.getValue());
+	        System.out.println("-----------------------------------------------------------------------------");
+	    }
 	}
 
 }
