7d5cf1e384ccc585aaa697dcfce1687e6ebdb43f
ali-abdelqawy <ali.abdelqawy@outlook.com>
2021-08-27 17:22:43 +0200

    add more fields to main.data.DNA class

--- src/main/data/DNA.java
+++ src/main/data/DNA.java
@@ -1,15 +1,23 @@
 package main.data;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import main.errors.ErrorGenerator;
 import main.validation.Validator;
 
 public class DNA {
-	final static String startCodon = "ATG";
-	final static String[] stopCodons = {"TAA", "TAG", "TGA"};
-	final static int codonLength = 3;
-	int totalGenes = 0;
+	private final static String startCodon = "ATG";
+	private final static String[] stopCodons = {"TAA", "TAG", "TGA"};
+	private final static int codonLength = 3;
+	private String fileName = "";
+	private String strand = "";
+	private ArrayList<String> genes = new ArrayList<String>(0);
+	
+	public DNA(String fileName, String strand) {
+		this.fileName = fileName;
+		this.strand = strand;
+	}
 	
 	public static HashMap<String, String> getStrandsByFiles(String folderName) {
 		if (isFolderNameEmpty(folderName)) {
