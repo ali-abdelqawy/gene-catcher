88485b102eec2e92eaf40f74c13ad0830196e082
ali-abdelqawy <ali.abdelqawy@outlook.com>
2021-09-12 21:44:11 +0200

    the algorithm implementation

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,6 +6,6 @@ public class Main {
 
 	public static void main(String[] args) {
 		final String folderName = "input";
-		GeneFinder gf  = new GeneFinder(folderName);
+		GeneFinder.run(folderName);
 	}
 }
--- src/main/algorithm/GeneFinder.java
+++ src/main/algorithm/GeneFinder.java
@@ -2,20 +2,95 @@ package main.algorithm;
 
 import java.util.ArrayList;
 
-import main.data.DNA;
-import main.data.Store;
+import main.input.DNA;
+import main.input.Store;
 import main.validation.Validator;
 
 public class GeneFinder {
 
-	private ArrayList<DNA> DNAs = null;
+	private DNA dna = null;
+	private String strand = "";
 	
-	public GeneFinder(String folderName) {
+	public GeneFinder(DNA dna) {
+		this.dna = dna;
+		this.strand = dna.getStrand();
+	}
+
+	public static void run(String folderName) {
 		if (!Validator.isFolderNameValid(folderName))
 			return;
 		
 		Store store = new Store(folderName);
-		DNAs = store.getDNAs();		
+			
+		ArrayList<DNA> DNAs = store.getDNAs();
+		for (DNA dna : DNAs) {
+			GeneFinder gf = new GeneFinder(dna);
+			gf.findAllGenes();
+			gf.printResult();
+		}
+	}
+	
+	private void findAllGenes() {
+		if (strand.isEmpty())
+			return;
+		
+		int fromIndex = 0, startCodonIndex = getStartCodonIndex(fromIndex);
+		
+		while (startCodonIndex != -1) {
+			String gene = findGene(startCodonIndex);
+			dna.addGene(gene);
+			// Update the start index to look for the next gene
+			fromIndex = startCodonIndex;
+			fromIndex += gene.isEmpty() ? DNA.codonLength : gene.length();
+			
+			startCodonIndex = getStartCodonIndex(fromIndex);
+		}
 	}
 
+	private int getStartCodonIndex(int fromIndex) {
+		return strand.indexOf(DNA.startCodon, fromIndex);
+	}
+	
+	private String findGene(int geneStart) {
+		int minIndex = getMinIndex(geneStart);
+		return minIndex != -1 ? strand.substring(geneStart, minIndex + DNA.codonLength) : "";
+	}
+	
+	private int getMinIndex(int geneStart) {
+		int minIndex = -1,
+			taaIndex = getStopCodonIndex(DNA.stopCodons.TAA.toString(), geneStart, geneStart),
+			tagIndex = getStopCodonIndex(DNA.stopCodons.TAG.toString(), geneStart, geneStart),
+			tgaIndex = getStopCodonIndex(DNA.stopCodons.TGA.toString(), geneStart, geneStart);
+		
+		minIndex = getMinIndexBetweenTwo(taaIndex, tagIndex);
+		minIndex = getMinIndexBetweenTwo(minIndex, tgaIndex);
+		
+		return minIndex;
+			
+	}
+	
+	private int getStopCodonIndex(String stopCodon, int geneStart, int fromIndex) {
+		int stopCodonIndex = strand.indexOf(stopCodon, fromIndex);
+		if (stopCodonIndex != -1)
+			return Validator.isGeneValid(geneStart, stopCodonIndex) ?
+				   stopCodonIndex :
+				   getStopCodonIndex(stopCodon, geneStart, stopCodonIndex + DNA.codonLength);
+		return -1;
+	}
+	
+	private static int getMinIndexBetweenTwo(int firstIndex, int secondIndex) {
+		return isSecondIndexSmaller(firstIndex, secondIndex) ? secondIndex : firstIndex;
+	}
+	
+	private static boolean isSecondIndexSmaller(int firstIndex, int secondIndex) {
+		return (firstIndex == -1 || (secondIndex != -1 && secondIndex < firstIndex));		
+	}
+	
+	private void printResult() {
+		System.out.println("filename -> " + "\"" + this.dna.getFileName() + "\"");
+		System.out.println("");
+		System.out.println("total genes = " + this.dna.getGenes().size());
+		System.out.println("----------------------------------");		
+	}
+	
 }
